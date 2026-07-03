c8f5d9694c66c5f8914d0276afd6e615e22e3b93
monokkai <egormayer08@mail.ru>
2026-07-03 20:38:44 +0200

    feat: init_users function

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ use warp::{Filter, Rejection, Reply, reject, reply};
 mod auth;
 mod error;
 
+type Result<T> = std::result::Result<T, error::Error>;
+type WebResult<T> = std::result::Result<T, Rejection>;
+type Users = Arc<HashMap<String, User>>;
+
 #[derive(Clone)]
 pub struct User {
     pub uid: String,
@@ -50,9 +54,45 @@ async fn main() {
         .or(admin_route)
         .recover(error::handle_rejection);
 
-    warp::serve(routes).run(([127, 0, 0 ,1], 8000))
+    warp::serve(routes).run(([127, 0, 0, 1], 8000))
 }
 
-fn with_users(users: Users) -> impl Filter<Extract> {
+fn with_users(users: Users) -> impl Filter<Extract = (Users,), Error = Infallible> + Clone {
+    warp::any().map(move || users.clone())
+}
 
+pub async fn login_handler(users: Users, body: LoginRequest) -> WebResult<impl Reply> {
+    match users
+        .iter()
+        .find(|(_uid, user) | user.email == body.email && user.pw == body.pw)
+        {
+            some((uid, user) => {
+                
+            })
+        }
+
+}
+
+fn init_users() -> HashMap<String, User> {
+    let mut map = HashMap::new();
+    map.insert(
+        String::from("1"),
+        User {
+            uid: String::from("1"),
+            email: String::from("user@gmail.com"),
+            pw: String::from("1234"),
+            role: String::from("User"),
+        },
+    );
+    map.insert(
+        String::from("2"),
+        User {
+            uid: String::from("2"),
+            email: String::from("admin@gmail.com"),
+            pw: String::from("admin"),
+            role: String::from("Admin"),
+        },
+    );
+
+    map
 }
